257984f17ecc242c20c238fac9aa193a57a6a80d
Nirfan206 <irfan905931@gmail.com>
2025-09-23 21:32:10 +0530

    Update CustomerReviews.js

--- client/src/pages/customer/CustomerReviews.js
+++ client/src/pages/customer/CustomerReviews.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../api'; 
+import api from '../../api';
 import { useNavigate } from 'react-router-dom';
 import StarRating from '../../components/StarRating'; // Import the StarRating component
 import { logout } from '../../utils/authUtils'; // Import logout utility
