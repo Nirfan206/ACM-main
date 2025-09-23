dd2fccebb4ba176f16a04038bfc4b29dc38e7b8b
Nirfan206 <irfan905931@gmail.com>
2025-09-23 21:34:32 +0530

    Update CareRequests.js

--- client/src/pages/customercare/CareRequests.js
+++ client/src/pages/customercare/CareRequests.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import api from '../api'; 
+import api from '../../api';
 
 function CareRequests() {
   const [requests, setRequests] = useState([]); // Callback requests
