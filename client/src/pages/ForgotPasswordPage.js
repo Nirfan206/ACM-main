558c192667ecd8c07435b28adbd145484cdf31b4
Nirfan206 <irfan905931@gmail.com>
2025-09-23 17:49:20 +0530

    Update ForgotPasswordPage.js

--- client/src/pages/ForgotPasswordPage.js
+++ client/src/pages/ForgotPasswordPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
+import api from '../api';
 import { Link } from 'react-router-dom';
 
 function ForgotPasswordPage() {
