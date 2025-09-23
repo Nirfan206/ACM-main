a357feb7e33d8d1a6e4a854159b34d221a8e698c
Nirfan206 <irfan905931@gmail.com>
2025-09-23 21:30:34 +0530

    Update AdminUsers.js

--- client/src/pages/admin/AdminUsers.js
+++ client/src/pages/admin/AdminUsers.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import api from '../api'; 
+import api from '../../api';
 
 function AdminUsers() {
   const [users, setUsers] = useState([]);
