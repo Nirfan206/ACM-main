119cdd0e8472fbd9a2c8838779ca238305009759
Nirfan206 <irfan905931@gmail.com>
2025-09-23 21:28:26 +0530

    Update AdminServices.js

--- client/src/pages/admin/AdminServices.js
+++ client/src/pages/admin/AdminServices.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import api from '../api'; 
+import api from '../../api';
 
 function AdminServices() {
   const [services, setServices] = useState([]);
