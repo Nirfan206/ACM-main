50e798764f457d0dbb6896c382511b5d0fbb609e
Nirfan206 <irfan905931@gmail.com>
2025-09-23 21:25:42 +0530

    Update AdminEmployeeStatus.js

--- client/src/pages/admin/AdminEmployeeStatus.js
+++ client/src/pages/admin/AdminEmployeeStatus.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../api'; // This import is correct
+import api from '../../api';// This import is correct
 import { useNavigate } from 'react-router-dom';
 
 function AdminEmployeeStatus() {
