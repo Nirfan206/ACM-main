08cd0a8358e4e75e34c9195304dd24a614c3dba8
Nirfan206 <irfan905931@gmail.com>
2025-09-23 17:59:57 +0530

    Update RegisterPage.js

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import api from '../api'; // You might need to adjust the path (e.g., '../../api')
 import { useNavigate, Link } from "react-router-dom";
 
 function RegisterPage() {
